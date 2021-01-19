33484f8529b2d51128dd786cb557adb8e318591b
Mike Henry <mhenry5@uci.edu>
2021-01-19 13:56:17 -0700

    make name better

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -19,7 +19,7 @@ import sphinx_rtd_theme
 
 # -- Project information -----------------------------------------------------
 
-project = 'SAMPL-docker-doc'
+project = 'SAMPL Docker Documentation'
 copyright = '2021, Mike Henry'
 author = 'Mike Henry'
 
